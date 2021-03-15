4ca4b68fd7a9e9390cc3a218e130098bab703584
Nathan Brown <nathan@nkbrown.us>
2021-03-14 21:05:06 -0700

    Improvements.
    Check for TCP connections to timeout and track transfered data.

--- TcpProxy.cs
+++ TcpProxy.cs
@@ -1,32 +1,72 @@
-﻿using System;
+﻿#nullable enable
+using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetProxy
 {
     internal class TcpProxy : IProxy
     {
-        public async Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string localIp)
+        /// <summary>
+        /// Milliseconds
+        /// </summary>
+        public int ConnectionTimeout { get; set; } = (4 * 60 * 1000);
+
+        public async Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string? localIp)
         {
-            //var clients = new ConcurrentDictionary<IPEndPoint, TcpClient>();
+            var connections = new ConcurrentBag<TcpConnection>();
 
             IPAddress localIpAddress = string.IsNullOrEmpty(localIp) ? IPAddress.IPv6Any : IPAddress.Parse(localIp);
-            var server = new System.Net.Sockets.TcpListener(new IPEndPoint(localIpAddress, localPort));
-            server.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
-            server.Start();
+            var localServer = new TcpListener(new IPEndPoint(localIpAddress, localPort));
+            localServer.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+            localServer.Start();
 
-            Console.WriteLine($"TCP proxy started {localPort} -> {remoteServerIp}|{remoteServerPort}");
+            Console.WriteLine($"TCP proxy started [{localIpAddress}]:{localPort} -> [{remoteServerIp}]:{remoteServerPort}");
+
+            var _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+
+                    var tempConnections = new List<TcpConnection>(connections.Count);
+                    while (connections.TryTake(out var connection))
+                    {
+                        tempConnections.Add(connection);
+                    }
+
+                    foreach (var tcpConnection in tempConnections)
+                    {
+                        if (tcpConnection.LastActivity + ConnectionTimeout < Environment.TickCount64)
+                        {
+                            tcpConnection.Stop();
+                        }
+                        else
+                        {
+                            connections.Add(tcpConnection);
+                        }
+                    }
+                }
+            });
+
             while (true)
             {
                 try
                 {
-                    var remoteClient = await server.AcceptTcpClientAsync();
-                    remoteClient.NoDelay = true;
-                    var ips = await Dns.GetHostAddressesAsync(remoteServerIp);
+                    var ips = await Dns.GetHostAddressesAsync(remoteServerIp).ConfigureAwait(false);
 
-                    new TcpClient(remoteClient, new IPEndPoint(ips.First(), remoteServerPort));
+                    var tcpConnection = await TcpConnection.AcceptTcpClientAsync(localServer,
+                            new IPEndPoint(ips.First(), remoteServerPort))
+                        .ConfigureAwait(false);
+                    tcpConnection.Run();
+                    connections.Add(tcpConnection);
                 }
                 catch (Exception ex)
                 {
@@ -38,51 +78,128 @@ namespace NetProxy
         }
     }
 
-    internal class TcpClient
+    internal class TcpConnection
     {
-        private System.Net.Sockets.TcpClient _remoteClient;
-        private IPEndPoint _clientEndpoint;
-        private IPEndPoint _remoteServer;
+        private readonly TcpClient _localServerConnection;
+        private readonly EndPoint? _sourceEndpoint;
+        private readonly IPEndPoint _remoteEndpoint;
+        private readonly TcpClient _forwardClient;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly EndPoint? _serverLocalEndpoint;
+        private EndPoint? _forwardLocalEndpoint;
+        private long _totalBytesForwarded;
+        private long _totalBytesResponded;
+        public long LastActivity { get; private set; } = Environment.TickCount64;
 
-        public TcpClient(System.Net.Sockets.TcpClient remoteClient, IPEndPoint remoteServer)
+        public static async Task<TcpConnection> AcceptTcpClientAsync(TcpListener tcpListener, IPEndPoint remoteEndpoint)
         {
-            _remoteClient = remoteClient;
-
-            _remoteServer = remoteServer;
-            client.NoDelay = true;
-            _clientEndpoint = (IPEndPoint) _remoteClient.Client.RemoteEndPoint;
-            Console.WriteLine($"Established {_clientEndpoint} => {remoteServer}");
-            Run();
+            var localServerConnection = await tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+            localServerConnection.NoDelay = true;
+            return new TcpConnection(localServerConnection, remoteEndpoint);
         }
 
-        public System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient();
+        private TcpConnection(TcpClient localServerConnection, IPEndPoint remoteEndpoint)
+        {
+            _localServerConnection = localServerConnection;
+            _remoteEndpoint = remoteEndpoint;
 
-        private void Run()
+            _forwardClient = new TcpClient {NoDelay = true};
+
+            _sourceEndpoint = _localServerConnection.Client.RemoteEndPoint;
+            _serverLocalEndpoint = _localServerConnection.Client.LocalEndPoint;
+        }
+
+        public void Run()
+        {
+            RunInternal(_cancellationTokenSource.Token);
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                _cancellationTokenSource.Cancel();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred while closing TcpConnection : {ex}");
+            }
+        }
+
+        private void RunInternal(CancellationToken cancellationToken)
         {
             Task.Run(async () =>
             {
                 try
                 {
-                    using (_remoteClient)
-                    using (client)
+                    using (_localServerConnection)
+                    using (_forwardClient)
                     {
-                        await client.ConnectAsync(_remoteServer.Address, _remoteServer.Port);
-                        var serverStream = client.GetStream();
-                        var remoteStream = _remoteClient.GetStream();
+                        await _forwardClient.ConnectAsync(_remoteEndpoint.Address, _remoteEndpoint.Port, cancellationToken).ConfigureAwait(false);
+                        _forwardLocalEndpoint = _forwardClient.Client.LocalEndPoint;
 
-                        await Task.WhenAny(remoteStream.CopyToAsync(serverStream),
-                            serverStream.CopyToAsync(remoteStream));
+                        Console.WriteLine($"Established TCP {_sourceEndpoint} => {_serverLocalEndpoint} => {_forwardLocalEndpoint} => {_remoteEndpoint}");
+
+                        using (var serverStream = _forwardClient.GetStream())
+                        using (var clientStream = _localServerConnection.GetStream())
+                        using (cancellationToken.Register(() =>
+                        {
+                            serverStream.Close();
+                            clientStream.Close();
+                        }, true))
+                        {
+                            await Task.WhenAny(
+                                CopyToAsync(clientStream, serverStream, 81920, Direction.Forward, cancellationToken),
+                                CopyToAsync(serverStream, clientStream, 81920, Direction.Responding, cancellationToken)
+                            ).ConfigureAwait(false);
+                        }
                     }
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    Console.WriteLine($"An exception occurred during TCP stream : {ex}");
                 }
                 finally
                 {
-                    Console.WriteLine($"Closed {_clientEndpoint} => {_remoteServer}");
-                    _remoteClient = null;
+                    Console.WriteLine($"Closed TCP {_sourceEndpoint} => {_serverLocalEndpoint} => {_forwardLocalEndpoint} => {_remoteEndpoint}. {_totalBytesForwarded} bytes forwarded, {_totalBytesResponded} bytes responded.");
                 }
             });
         }
+
+        private async Task CopyToAsync(Stream source, Stream destination, int bufferSize = 81920, Direction direction = Direction.Unknown, CancellationToken cancellationToken = default)
+        {
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = await source.ReadAsync(new Memory<byte>(buffer), cancellationToken).ConfigureAwait(false);
+                    if (bytesRead == 0) break;
+                    LastActivity = Environment.TickCount64;
+                    await destination.WriteAsync(new ReadOnlyMemory<byte>(buffer, 0, bytesRead), cancellationToken).ConfigureAwait(false);
+
+                    switch (direction)
+                    {
+                        case Direction.Forward:
+                            Interlocked.Add(ref _totalBytesForwarded, bytesRead);
+                            break;
+                        case Direction.Responding:
+                            Interlocked.Add(ref _totalBytesResponded, bytesRead);
+                            break;
+                    }
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+    }
+
+    internal enum Direction
+    {
+        Unknown = 0,
+        Forward,
+        Responding,
     }
 }
--- UdpProxy.cs
+++ UdpProxy.cs
@@ -1,48 +1,62 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetProxy
 {
     internal class UdpProxy : IProxy
     {
-        public async Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string localIp = null)
-        {
-            var clients = new ConcurrentDictionary<IPEndPoint, UdpClient>();
+        /// <summary>
+        /// Milliseconds
+        /// </summary>
+        public int ConnectionTimeout { get; set; } = (4 * 60 * 1000);
 
-            var server = new System.Net.Sockets.UdpClient(AddressFamily.InterNetworkV6);
-            server.Client.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+        public async Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string? localIp = null)
+        {
+            var connections = new ConcurrentDictionary<IPEndPoint, UdpConnection>();
+            var remoteServerEndPoint = new IPEndPoint(IPAddress.Parse(remoteServerIp), remoteServerPort);
+
+            var localServer = new UdpClient(AddressFamily.InterNetworkV6);
+            localServer.Client.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
             IPAddress localIpAddress = string.IsNullOrEmpty(localIp) ? IPAddress.IPv6Any : IPAddress.Parse(localIp);
-            server.Client.Bind(new IPEndPoint(localIpAddress, localPort));
-            Console.WriteLine($"proxy started UDP:{localIpAddress}|{localPort} -> {remoteServerIp}|{remoteServerPort}");
+            localServer.Client.Bind(new IPEndPoint(localIpAddress, localPort));
+
+            Console.WriteLine($"UDP proxy started [{localIpAddress}]:{localPort} -> [{remoteServerIp}]:{remoteServerPort}");
+
             var _ = Task.Run(async () =>
             {
                 while (true)
                 {
-                    await Task.Delay(10000);
-                    foreach (var client in clients.ToArray())
+                    await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+                    foreach (var connection in connections.ToArray())
                     {
-                        if (client.Value.lastActivity + TimeSpan.FromSeconds(60) < DateTime.UtcNow)
+                        if (connection.Value.LastActivity + ConnectionTimeout < Environment.TickCount64)
                         {
-                            UdpClient c;
-                            clients.TryRemove(client.Key, out c);
-                            client.Value.Stop();
+                            connections.TryRemove(connection.Key, out UdpConnection? c);
+                            connection.Value.Stop();
                         }
                     }
                 }
             });
+
             while (true)
             {
                 try
                 {
-                    var message = await server.ReceiveAsync();
-                    var endpoint = message.RemoteEndPoint;
-                    var client = clients.GetOrAdd(endpoint,
-                        ep => new UdpClient(server, endpoint,
-                            new IPEndPoint(IPAddress.Parse(remoteServerIp), remoteServerPort)));
-                    await client.SendToServer(message.Buffer);
+                    var message = await localServer.ReceiveAsync().ConfigureAwait(false);
+                    var sourceEndPoint = message.RemoteEndPoint;
+                    var client = connections.GetOrAdd(sourceEndPoint,
+                        ep =>
+                        {
+                            var udpConnection = new UdpConnection(localServer, sourceEndPoint, remoteServerEndPoint);
+                            udpConnection.Run();
+                            return udpConnection;
+                        });
+                    await client.SendToServerAsync(message.Buffer).ConfigureAwait(false);
                 }
                 catch (Exception ex)
                 {
@@ -52,59 +66,68 @@ namespace NetProxy
         }
     }
 
-    internal class UdpClient
+    internal class UdpConnection
     {
-        private readonly System.Net.Sockets.UdpClient _server;
+        private readonly UdpClient _localServer;
+        private readonly UdpClient _forwardClient;
+        public long LastActivity { get; private set; } = Environment.TickCount64;
+        private readonly IPEndPoint _sourceEndpoint;
+        private readonly IPEndPoint _remoteEndpoint;
+        private readonly EndPoint? _serverLocalEndpoint;
+        private EndPoint? _forwardLocalEndpoint;
+        private bool _isRunning;
+        private long _totalBytesForwarded;
+        private long _totalBytesResponded;
+        private readonly TaskCompletionSource<bool> _forwardConnectionBindCompleted = new TaskCompletionSource<bool>();
 
-        public UdpClient(System.Net.Sockets.UdpClient server, IPEndPoint clientEndpoint, IPEndPoint remoteServer)
+        public UdpConnection(UdpClient localServer, IPEndPoint sourceEndpoint, IPEndPoint remoteEndpoint)
         {
-            _server = server;
+            _localServer = localServer;
+            _serverLocalEndpoint = _localServer.Client.LocalEndPoint;
 
             _isRunning = true;
-            _remoteServer = remoteServer;
-            _clientEndpoint = clientEndpoint;
-            Console.WriteLine($"Established {clientEndpoint} => {remoteServer}");
-            Run();
+            _remoteEndpoint = remoteEndpoint;
+            _sourceEndpoint = sourceEndpoint;
+
+            _forwardClient = new UdpClient(AddressFamily.InterNetworkV6);
+            _forwardClient.Client.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
         }
 
-        public readonly System.Net.Sockets.UdpClient client = new System.Net.Sockets.UdpClient();
-        public DateTime lastActivity = DateTime.UtcNow;
-        private readonly IPEndPoint _clientEndpoint;
-        private readonly IPEndPoint _remoteServer;
-        private bool _isRunning;
-        private readonly TaskCompletionSource<bool> _tcs = new TaskCompletionSource<bool>();
-
-        public async Task SendToServer(byte[] message)
+        public async Task SendToServerAsync(byte[] message)
         {
-            lastActivity = DateTime.UtcNow;
+            LastActivity = Environment.TickCount64;
 
-            await _tcs.Task;
-            var sent = await client.SendAsync(message, message.Length, _remoteServer);
-            Console.WriteLine(
-                $"{sent} bytes sent from a client message of {message.Length} bytes from {_clientEndpoint} to {_remoteServer}");
+            await _forwardConnectionBindCompleted.Task.ConfigureAwait(false);
+            var sent = await _forwardClient.SendAsync(message, message.Length, _remoteEndpoint).ConfigureAwait(false);
+            Interlocked.Add(ref _totalBytesForwarded, sent);
         }
 
-        private void Run()
+        public void Run()
         {
             Task.Run(async () =>
             {
-                client.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
-                _tcs.SetResult(true);
-                using (client)
+                using (_forwardClient)
                 {
+                    _forwardClient.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
+                    _forwardLocalEndpoint = _forwardClient.Client.LocalEndPoint;
+                    _forwardConnectionBindCompleted.SetResult(true);
+                    Console.WriteLine($"Established UDP {_sourceEndpoint} => {_serverLocalEndpoint} => {_forwardLocalEndpoint} => {_remoteEndpoint}");
+
                     while (_isRunning)
                     {
                         try
                         {
-                            var result = await client.ReceiveAsync();
-                            lastActivity = DateTime.UtcNow;
-                            var sent = await _server.SendAsync(result.Buffer, result.Buffer.Length, _clientEndpoint);
-                            Console.WriteLine(
-                                $"{sent} bytes sent from a return message of {result.Buffer.Length} bytes from {_remoteServer} to {_clientEndpoint}");
+                            var result = await _forwardClient.ReceiveAsync().ConfigureAwait(false);
+                            LastActivity = Environment.TickCount64;
+                            var sent = await _localServer.SendAsync(result.Buffer, result.Buffer.Length, _sourceEndpoint).ConfigureAwait(false);
+                            Interlocked.Add(ref _totalBytesResponded, sent);
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine($"An exception occurred while receiving a server datagram : {ex}");
+                            if (_isRunning)
+                            {
+                                Console.WriteLine($"An exception occurred while receiving a server datagram : {ex}");
+                            }
                         }
                     }
                 }
@@ -113,8 +136,16 @@ namespace NetProxy
 
         public void Stop()
         {
-            Console.WriteLine($"Closed {_clientEndpoint} => {_remoteServer}");
-            _isRunning = false;
+            try
+            {
+                Console.WriteLine($"Closed UDP {_sourceEndpoint} => {_serverLocalEndpoint} => {_forwardLocalEndpoint} => {_remoteEndpoint}. {_totalBytesForwarded} bytes forwarded, {_totalBytesResponded} bytes responded.");
+                _isRunning = false;
+                _forwardClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred while closing UdpConnection : {ex}");
+            }
         }
     }
 }
